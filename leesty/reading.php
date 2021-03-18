f9a8434b61c8e1a88fe99976460189f16a253f34
SydNS <40373744+SydNS@users.noreply.github.com>
2021-03-18 16:15:21 -0700

    added a delete button for the inform

--- leesty/reading.php
+++ leesty/reading.php
@@ -4,22 +4,24 @@ include '../includes/dbconn.php';
 
 if (isset($conn)) {
 
-        $sqlqueryafterposting = "SELECT * FROM posts";
-        $qryresults=mysqli_query($conn,$sqlqueryafterposting);
+    $sqlqueryafterposting = "SELECT * FROM posts";
 
-        if (mysqli_num_rows($qryresults)>0){
-            while ($row=mysqli_fetch_assoc($qryresults)){
+    $qryresults = mysqli_query($conn, $sqlqueryafterposting);
 
+    if (mysqli_num_rows($qryresults) > 0) {
+        while ($row = mysqli_fetch_assoc($qryresults)) {
 
+            $rid=$row['id'];
+            $sqlquerydeletingpost = "DELETE FROM posts WHERE id='$rid'";
+            $qrydelete = mysqli_query($conn, $sqlquerydeletingpost);
 
-                echo "<p> id is " .$row['id'] . " title is " .$row['title'] . " details is " .$row['details'] . "date poseted is " .$row['date'] . " <br> </p>";
-            }
+            echo "<p> id is " . $row['id'] . " title is " . $row['title'] . " details is " . $row['details'] . "date poseted is " . $row['date'] . " <button>Del<?php $qrydelete;?></button><br> </p>";
         }
+    }
 
 
-
-    }else{
-        echo "Nothing is Post ".mysqli_error($conn);
+} else {
+    echo "Nothing is Post " . mysqli_error($conn);
 
 
 }
