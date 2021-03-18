6db71cee1ef5aaa59f0b58db9ad9bbd208c3db07
SydNS <40373744+SydNS@users.noreply.github.com>
2021-03-18 14:59:51 -0700

    executing the query in for inserting the data and also understood the mysqli_query

--- leesty/posting.php
+++ leesty/posting.php
@@ -1,2 +1,17 @@
 <?php
+;
+include '../includes/dbconn.php';
 
+$title="Marriage";
+$details="I am not the one whole told you not to get Married";
+$author="Bruce";
+
+$sqlquery = "INSERT INTO posts('id', 'title', 'details', 'author', 'date')  VALUES ($title , $details,$author  )";
+
+if (isset($conn)) {
+    $qryresults=mysqli_query($conn,$sqlquery);
+    if ($qryresults){
+        echo "Success Pos"
+    }
+}
+?>
