561b15163b4c521aba739c21053945fd8d4cc3dc
SydNS <40373744+SydNS@users.noreply.github.com>
2021-03-18 14:58:55 -0700

    added a check for the connection to be valid

--- includes/dbconn.php
+++ includes/dbconn.php
@@ -3,4 +3,11 @@ $dbname="leesty";
 $dbuser="root";
 $dbpass="";
 $dblink="localhost";
-$conn=mysqli_connect($dblink,$dbname,$dbpass,$dbuser);
+$conn=mysqli_connect($dblink,$dbuser,$dbpass,$dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
