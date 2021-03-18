a52ba1e661fb71e3a469f02b9da152ff7612b83b
SydNS <40373744+SydNS@users.noreply.github.com>
2021-03-18 15:35:45 -0700

    Added a form  to pull data from the POSt ethod in the formthat put the Data in a Form in an array entitled the $_POST['input tag name']

--- leesty/posting.php
+++ leesty/posting.php
@@ -2,19 +2,55 @@
 ;
 include '../includes/dbconn.php';
 
-$posttittle="Marriage2";
-$details="I am not the one whole told you not to get Married 2";
-$author="Bruce 2";
+if (isset($_POST['subbutton'])){
 
-$sqlquery = "INSERT INTO posts( title, details, author)  VALUES ('$posttittle' , '$details','$author')";
+    $posttittle=$_POST['title'];
+    $details=$_POST['details'];
+    $author=$_POST['author'];
+//    $posttittle=$_POST['title'];
+//    $posttittle="Marriage2";
+//    $details="I am not the one whole told you not to get Married 2";
+//    $author="Bruce 2";
 
-if (isset($conn)) {
-    $qryresults=mysqli_query($conn,$sqlquery);
-    if ($qryresults){
-        echo "Success Post";
-    }else{
-        echo "Nothing is Post ".mysqli_error($conn);
+    $sqlquery = "INSERT INTO posts( title, details, author)  VALUES ('$posttittle' , '$details','$author')";
+
+    if (isset($conn)) {
+        $qryresults=mysqli_query($conn,$sqlquery);
+        if ($qryresults){
+            echo "Success Post";
+            $sqlquery = "SELECT * FROM posts";
+
+
+
+        }else{
+            echo "Nothing is Post ".mysqli_error($conn);
+        }
+
     }
-
 }
+
+
 ?>
+
+<html>
+<title>
+    Posting Page
+</title>
+<head>
+    <link href="" rel="stylesheet">
+</head>
+<body>
+<form method="post">
+    <label>Title</label> <br>
+    <input name="title" type="text" required> <br>
+    <label>Details</label> <br>
+    <input name="details" type="text" required> <br>
+    <label>Author</label> <br>
+    <input name="author" type="text" required> <br>
+    <input name="subbutton" type="submit">
+</form>
+
+</body>
+
+
+</html>
