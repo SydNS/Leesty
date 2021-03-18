aa0af2e5048c1936d5f715f6bef9775f24b2e6d6
SydNS <40373744+SydNS@users.noreply.github.com>
2021-03-18 14:36:43 -0700

    code for creating a table for posting

---
+++ includes/leesty.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `leesty`.`posts` ( `id` INT(3) NOT NULL AUTO_INCREMENT , `title` VARCHAR(100) NOT NULL , `details` VARCHAR(200) NOT NULL , `author` VARCHAR(20) NOT NULL , `date` DATE NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
