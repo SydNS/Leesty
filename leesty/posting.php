27389aaa35af7e7c254817c64370a02346c920be
SydNS <40373744+SydNS@users.noreply.github.com>
2021-03-18 15:15:51 -0700

    i debugged the query syntax that was causing errors

--- leesty/posting.php
+++ leesty/posting.php
@@ -2,16 +2,19 @@
 ;
 include '../includes/dbconn.php';
 
-$title="Marriage";
-$details="I am not the one whole told you not to get Married";
-$author="Bruce";
+$posttittle="Marriage2";
+$details="I am not the one whole told you not to get Married 2";
+$author="Bruce 2";
 
-$sqlquery = "INSERT INTO posts('id', 'title', 'details', 'author', 'date')  VALUES ($title , $details,$author  )";
+$sqlquery = "INSERT INTO posts( title, details, author)  VALUES ('$posttittle' , '$details','$author')";
 
 if (isset($conn)) {
     $qryresults=mysqli_query($conn,$sqlquery);
     if ($qryresults){
         echo "Success Post";
+    }else{
+        echo "Nothing is Post ".mysqli_error($conn);
     }
+
 }
 ?>
